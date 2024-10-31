b1f6ddca697aaf71609960d60d0b6a2aa2fe20ef
Felipe <78973333+felipe-monteiro16@users.noreply.github.com>
2024-10-31 16:15:33 -0300

    Update exc001.c

--- exc001.c
+++ exc001.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define max 10
 
+
+void main (){
+    int nota[max], cont = 0, t, soma = 0; 
+    float media;
+    
+    srand(time(NULL));
+    for(t=0; t<max; ++t){
+        nota[t] = rand()%10;
+        printf("%d\n", nota[t]);
+    }
+    for(t=0; t<max; ++t){
+        soma += nota[t];
+        if (nota[t]<6){
+            cont++;
+        }
+    }
+    media = (float)soma/10;
+    printf("A soma é %d, e A média é %.2f e a quantia de reprovados é %d", soma, media, cont);
+    
+}
