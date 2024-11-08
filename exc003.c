bf419eb297045c243eb18a4f40e9c47ebb3e87ee
Felipe <78973333+felipe-monteiro16@users.noreply.github.com>
2024-11-08 16:17:53 -0300

    Update exc003.c

--- exc003.c
+++ exc003.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-Incompleto!! O desafio é para a pessoa me informar um valor e ser gerado
+O desafio é para a pessoa me informar um valor e ser gerado
 a matriz correspondente, exemplo: 
 Digite um valor: 3
 1 1 1 1 1 
@@ -9,35 +9,41 @@ Digite um valor: 3
 1 1 1 1 1
 
 E assim com qualquer valor.
-
 *******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #define max 30
-void main()
-{
+
+void main() {
     int num, cont, lista[max], c, d, i, j, a, cont1 = 0;
     printf("Digite um número para a matriz: ");
     scanf("%d", &num);
-    
-    cont = (2 * (num-1)) + 1;
-    for(c=1; c<num+1; ++c){
-        lista[c-1] = c;
-    } 
-    int tamanho = sizeof(lista) / sizeof(lista[0]);
-    for(i=1; i<cont+1; ++i){ 
-        for(j=1; j<cont+1; ++j){
+    cont = (2 * (num-1)) + 1; // DEFININDO ALTURA/LARGURA DA MATRIZ 
+    for(i=1; i<cont+1; ++i){         // LINHA DA MATRIZ
+        for(j=1; j<cont+1; ++j){    // COLUNA DA MATRIZ
             cont1=cont;
-            for(d=0; d<tamanho; d++){
-                if(i==lista[d] || i==cont1 || j==lista[d] || j==cont1){
-                    a = lista[d];
+            for(d=0; d<num; d++){ // DEFININDO O VALOR A SER IMPRIMIDO
+                if(i==d+1 || i==cont1 || j==d+1 || j==cont1){
+                    a = d+1;
                     break;
                 } else {
                     cont1 = cont1-1;
-                }
+                }     
             }           
             printf("%d ", a);
         }
         printf("\n");
     }
 }
+/* 
+Ali encima na parte "DEFININDO O VALOR A SER IMPRIMIDO"
+tá meio difícil de entender mas vamo la:
+Foi identificado um padrão nos valores a serem escritos
+por exemplo no caso num=3, é digitado o número 1 quando
+está na linha *1* ou coluna 1, ou na linha *5* ou coluna 5
+pra digitar o número 2 não é muito diferente, linha 2
+ou coluna 2, ou linha 4 ou coluna 4
+Percebe-se que da mesma forma que o primeiro número aumenta
+de 1 em 1, o outro reduz de 1 em 1, Nisso foi só criar um
+um laço com a variável "d" que aumenta e a "cont1" que reduz
+colocando dentro a estrutura condicional.*/
