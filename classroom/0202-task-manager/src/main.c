5aa5a790226bf57cf6b13e039e3e2db454fb0213
Felipe Monteiro <felipemmateuss@gmail.com>
2025-08-27 17:03:10 -0300

    finished task-manager

--- classroom/0202-task-manager/src/main.c
+++ classroom/0202-task-manager/src/main.c
@@ -16,11 +16,10 @@ void clearTerminal() {
 
 
 void pressEnter() {
-    printf("\n\nPress Enter to continue...");
-    while (getchar() != '\n');
-    getchar();
+    printf("Press Enter to continue...");
+    int c;  
+    while ((c = getchar()) != '\n' && c != EOF);
     clearTerminal();
-
 }
 
 
@@ -44,53 +43,145 @@ void addTask() {
     FILE *fp;
     char task[100];
     
-    fp = fopen(FILE_PATH, "a");
-
     clearTerminal();
     printf("\n");
     printf("==================================\n");
     printf("             NEW TASK             \n");
     printf("==================================\n");
-    printf("Text:");
-
+    printf("- ");
+    
     // Get user input for the task
-    while (getchar() != '\n');
     fgets(task, sizeof(task), stdin);
-
+    
     // Remove the newline character from the input
     task[strcspn(task, "\n")] = 0;
-
+    
     // Open the file in append mode
+    fp = fopen(FILE_PATH, "a");
     if (fp == NULL) {
         printf("Error opening file.\n");
         return;
     }
     
+    // Write the file
+    fprintf(fp, "- %s\n", task);
+    fclose(fp);
+
+    printf("\n Task added successfully.\n");
+    printf("==================================\n");
+}
+
+
+void showTasks() {
+    FILE *fp;
+    char buffer[256];
+    fp = fopen(FILE_PATH, "r");
+    
+    clearTerminal();
+    printf("==================================\n");
+    printf("              ALL TASKS            \n");
+    printf("==================================\n");
+    
+    // Valid if tasks file exists
+    if (fp == NULL) {
+        printf("No tasks found.\n ");
+        return;
+    }
+
+    // Print the file content
+    int num = 0;
+    while(fgets(buffer, sizeof(buffer), fp)) {
+        printf("[%d] %s", num, buffer);
+        num++;
+    }
+    fclose(fp);
+
+    printf("\n==================================\n");
+}
+
+
+void deleteTask() {
+    int option, i = 0, j = 0, c;
+    char buffer[256], tasks[50][100];
+    FILE *fp;
+    
+    // Show tasks for deletion
+    showTasks();
+    printf("Delete Task: ");
+    if (scanf("%d", &option) != 1) return;
+
+    // Clear buffer
+    while ((c = getchar()) != '\n' && c != EOF);
+
+    // Open file
+    fp = fopen(FILE_PATH, "r");
+    if (fp == NULL) {
+        printf("No tasks found");
+        return;
+    }
+
+    // Copy tasks to an array
+    while(fgets(buffer, sizeof(buffer), fp)) {
+        strcpy(tasks[i], buffer);
+        i++;
+    }
+    if (option>=i) return; 
+    fclose(fp);
+
+    // Overwrite file without selected task
+    fp = fopen(FILE_PATH, "w");
+    for(j=0; j<i; j++){
+        if (j == option) continue;
+        fputs(tasks[j], fp);
+    }
+    
+    printf("\n==================================\n");
+    printf("Task [%d] removed successfully.\n", option);
+    printf("==================================\n");
+    fclose(fp);
+
 
 }
 
 
-void finish_code() {
+void finishCode() {
     clearTerminal();
-    printf("Finishing...");
+    printf("==================================\n");
+    printf("Thanks for using task manager!\n");
+    printf("Finishing...\n");
+    printf("==================================\n");
+    exit(0);
 }
 
 
 int main() {
     int option;
 
-    showMenu();
-    if (scanf("%d", &option) != 1) return 1;
-
-    switch (option)
+    while (1)
     {
-    case 1:
-        addTask();
-        break;
-    
-    default:
-        finish_code();
-        break;
+        showMenu();
+        if (scanf("%d", &option) != 1) return 1;
+        int c;  
+        while ((c = getchar()) != '\n' && c != EOF);
+        switch (option)
+        {
+        case 1:
+            addTask();
+            pressEnter();
+            break;
+        case 2:
+            showTasks();
+            pressEnter();
+            break;
+        case 3:
+            deleteTask();
+            pressEnter();
+            break;
+        default:
+            finishCode();
+            break;
+        }
     }
+    
     return 0;
 }
